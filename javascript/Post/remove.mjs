80e8546f967a9046925e97ffb3b893ef44ace538
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-11-10 09:13:27 +0100

    try to fix error

--- javascript/Post/remove.mjs
+++ javascript/Post/remove.mjs
@@ -6,19 +6,17 @@ const action = "/api/v1/social/posts";
 export async function removePost(id) {
   const removePostsURL = `${API_BASE_URL}${action}/${id}`;
 
-  try {
-    const response = await fetchTokens(removePostsURL, {
-      method: "DELETE",
-    });
-    const remove = await response.json();
+  const response = await fetchTokens(removePostsURL, {
+    method: "DELETE",
+  });
+  const remove = await response.json();
 
-    if (response.ok) {
-      redirectToPost();
-      return remove;
-    } else {
-      alert("You are not allowed to delete this post");
-    }
-  } catch (error) {}
+  if (response.ok) {
+    redirectToPost();
+    return remove;
+  } else {
+    alert("You are not allowed to delete this post");
+  }
 }
 
 function redirectToPost() {
